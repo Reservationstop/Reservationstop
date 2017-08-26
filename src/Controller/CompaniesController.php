28ce016c7c98a283ac2de92119f8b3fa13ce9793
trompitrompi <31183145+trompitrompi@users.noreply.github.com>
2017-08-26 11:33:27 +0300

    some work (#19)

--- src/Controller/CompaniesController.php
+++ src/Controller/CompaniesController.php
@@ -1,11 +1,11 @@
 <?php
+
 namespace App\Controller;
 
 use App\Controller\AppController;
 use Cake\Controller\Controller;
 use Cake\Event\Event;
 use Cake\Core\Configure;
-
 
 /**
  * Companies Controller
@@ -14,159 +14,151 @@ use Cake\Core\Configure;
  *
  * @method \App\Model\Entity\Company[] paginate($object = null, array $settings = [])
  */
-class CompaniesController extends AppController
-{
-	   /**
-     * Before render callback.
-     *
-     * @param \Cake\Event\Event $event The beforeRender event.
-     * @return \Cake\Network\Response|null|void
-     */
-    public function beforeRender(Event $event)
-    {
+class CompaniesController extends AppController {
 
+	/**
+	 * Before render callback.
+	 *
+	 * @param \Cake\Event\Event $event The beforeRender event.
+	 * @return \Cake\Network\Response|null|void
+	 */
+	public function beforeRender(Event $event) {
+		parent::beforeRender($event);
 		$this->set('theme', Configure::read('Theme'));
-		
-		
-        if (!array_key_exists('_serialize', $this->viewVars) &&
-            in_array($this->response->type(), ['application/json', 'application/xml'])
-        ) {
-            $this->set('_serialize', true);
-        }
-    }
+		if (!array_key_exists('_serialize', $this->viewVars) &&
+				in_array($this->response->type(), ['application/json', 'application/xml'])
+		) {
+			$this->set('_serialize', true);
+		}
+	}
 
-    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|void
-     */
-    public function index()
-    {	
+	/**
+	 * Index method
+	 *
+	 * @return \Cake\Http\Response|void
+	 */
+	public function index() {
 		$this->viewBuilder()->theme('AdminLTE');
-        $this->paginate = [
-            'contain' => ['Types', 'Countries', 'Statuses', 'Timezones', 'Currencies', 'Languages', 'Areas']
-        ];
-        $companies = $this->paginate($this->Companies);
+		$this->paginate = [
+			'contain' => ['Types', 'Countries', 'Statuses', 'Timezones', 'Currencies', 'Languages', 'Areas']
+		];
+		$companies = $this->paginate($this->Companies);
 
-        $this->set(compact('companies'));
-        $this->set('_serialize', ['companies']);
-    }
+		$this->set(compact('companies'));
+		$this->set('_serialize', ['companies']);
+	}
 
-	    /**
-     * Index method
-     *
-     * @return \Cake\Http\Response|void
-     */
-    public function frontend()
-    {
-		//$this->viewBuilder()->theme('AdminLTE');
+	/**
+	 * Index method
+	 *
+	 * @return \Cake\Http\Response|void
+	 */
+	public function frontend() {
 		$this->viewBuilder()->setLayout('default');
-        $this->paginate = [
-            'contain' => ['Types', 'Countries', 'Statuses', 'Timezones', 'Currencies', 'Languages', 'Areas']
-        ];
-        $companies = $this->paginate($this->Companies);
+		$this->paginate = [
+			'contain' => ['Types', 'Countries', 'Statuses', 'Timezones', 'Currencies', 'Languages', 'Areas']
+		];
+		$companies = $this->paginate($this->Companies);
 
-        $this->set(compact('companies'));
-        $this->set('_serialize', ['companies']);
-    }
-    /**
-     * View method
-     *
-     * @param string|null $id Company id.
-     * @return \Cake\Http\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
+		$this->set(compact('companies'));
+		$this->set('_serialize', ['companies']);
+	}
+
+	/**
+	 * View method
+	 *
+	 * @param string|null $id Company id.
+	 * @return \Cake\Http\Response|void
+	 * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+	 */
+	public function view($id = null) {
 		$this->viewBuilder()->theme('AdminLTE');
-        $company = $this->Companies->get($id, [
-            'contain' => ['Types', 'Countries', 'Statuses', 'Timezones', 'Currencies', 'Languages', 'Areas']
-        ]);
+		$company = $this->Companies->get($id, [
+			'contain' => ['Types', 'Countries', 'Statuses', 'Timezones', 'Currencies', 'Languages', 'Areas']
+		]);
 
-        $this->set('company', $company);
-        $this->set('_serialize', ['company']);
-    }
+		$this->set('company', $company);
+		$this->set('_serialize', ['company']);
+	}
 
-    /**
-     * Add method
-     *
-     * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
+	/**
+	 * Add method
+	 *
+	 * @return \Cake\Http\Response|null Redirects on successful add, renders view otherwise.
+	 */
+	public function add() {
 		$this->viewBuilder()->theme('AdminLTE');
-        $company = $this->Companies->newEntity();
-        if ($this->request->is('post')) {
-            $company = $this->Companies->patchEntity($company, $this->request->getData());
-            if ($this->Companies->save($company)) {
-                $this->Flash->success(__('The company has been saved.'));
+		$company = $this->Companies->newEntity();
+		if ($this->request->is('post')) {
+			$company = $this->Companies->patchEntity($company, $this->request->getData());
+			if ($this->Companies->save($company)) {
+				$this->Flash->success(__('The company has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The company could not be saved. Please, try again.'));
-        }
-        $types = $this->Companies->Types->find('list', ['limit' => 200]);
-        $countries = $this->Companies->Countries->find('list', ['limit' => 200]);
-        $statuses = $this->Companies->Statuses->find('list', ['limit' => 200]);
-        $timezones = $this->Companies->Timezones->find('list', ['limit' => 200]);
-        $currencies = $this->Companies->Currencies->find('list', ['limit' => 200]);
-        $languages = $this->Companies->Languages->find('list', ['limit' => 200]);
-        $areas = $this->Companies->Areas->find('list', ['limit' => 200]);
-        $this->set(compact('company', 'types', 'countries', 'statuses', 'timezones', 'currencies', 'languages', 'areas'));
-        $this->set('_serialize', ['company']);
-    }
+				return $this->redirect(['action' => 'index']);
+			}
+			$this->Flash->error(__('The company could not be saved. Please, try again.'));
+		}
+		$types = $this->Companies->Types->find('list', ['limit' => 200]);
+		$countries = $this->Companies->Countries->find('list', ['limit' => 200]);
+		$statuses = $this->Companies->Statuses->find('list', ['limit' => 200]);
+		$timezones = $this->Companies->Timezones->find('list', ['limit' => 200]);
+		$currencies = $this->Companies->Currencies->find('list', ['limit' => 200]);
+		$languages = $this->Companies->Languages->find('list', ['limit' => 200]);
+		$areas = $this->Companies->Areas->find('list', ['limit' => 200]);
+		$this->set(compact('company', 'types', 'countries', 'statuses', 'timezones', 'currencies', 'languages', 'areas'));
+		$this->set('_serialize', ['company']);
+	}
 
-    /**
-     * Edit method
-     *
-     * @param string|null $id Company id.
-     * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {	
+	/**
+	 * Edit method
+	 *
+	 * @param string|null $id Company id.
+	 * @return \Cake\Http\Response|null Redirects on successful edit, renders view otherwise.
+	 * @throws \Cake\Network\Exception\NotFoundException When record not found.
+	 */
+	public function edit($id = null) {
 		$this->viewBuilder()->theme('AdminLTE');
-        $company = $this->Companies->get($id, [
-            'contain' => []
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $company = $this->Companies->patchEntity($company, $this->request->getData());
-            if ($this->Companies->save($company)) {
-                $this->Flash->success(__('The company has been saved.'));
+		$company = $this->Companies->get($id, [
+			'contain' => []
+		]);
+		if ($this->request->is(['patch', 'post', 'put'])) {
+			$company = $this->Companies->patchEntity($company, $this->request->getData());
+			if ($this->Companies->save($company)) {
+				$this->Flash->success(__('The company has been saved.'));
 
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The company could not be saved. Please, try again.'));
-        }
-        $types = $this->Companies->Types->find('list', ['limit' => 200]);
-        $countries = $this->Companies->Countries->find('list', ['limit' => 200]);
-        $statuses = $this->Companies->Statuses->find('list', ['limit' => 200]);
-        $timezones = $this->Companies->Timezones->find('list', ['limit' => 200]);
-        $currencies = $this->Companies->Currencies->find('list', ['limit' => 200]);
-        $languages = $this->Companies->Languages->find('list', ['limit' => 200]);
-        $areas = $this->Companies->Areas->find('list', ['limit' => 200]);
-        $this->set(compact('company', 'types', 'countries', 'statuses', 'timezones', 'currencies', 'languages', 'areas'));
-        $this->set('_serialize', ['company']);
-    }
+				return $this->redirect(['action' => 'index']);
+			}
+			$this->Flash->error(__('The company could not be saved. Please, try again.'));
+		}
+		$types = $this->Companies->Types->find('list', ['limit' => 200]);
+		$countries = $this->Companies->Countries->find('list', ['limit' => 200]);
+		$statuses = $this->Companies->Statuses->find('list', ['limit' => 200]);
+		$timezones = $this->Companies->Timezones->find('list', ['limit' => 200]);
+		$currencies = $this->Companies->Currencies->find('list', ['limit' => 200]);
+		$languages = $this->Companies->Languages->find('list', ['limit' => 200]);
+		$areas = $this->Companies->Areas->find('list', ['limit' => 200]);
+		$this->set(compact('company', 'types', 'countries', 'statuses', 'timezones', 'currencies', 'languages', 'areas'));
+		$this->set('_serialize', ['company']);
+	}
 
-    /**
-     * Delete method
-     *
-     * @param string|null $id Company id.
-     * @return \Cake\Http\Response|null Redirects to index.
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function delete($id = null)
-    {
+	/**
+	 * Delete method
+	 *
+	 * @param string|null $id Company id.
+	 * @return \Cake\Http\Response|null Redirects to index.
+	 * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
+	 */
+	public function delete($id = null) {
 		$this->viewBuilder()->theme('AdminLTE');
-        $this->request->allowMethod(['post', 'delete']);
-        $company = $this->Companies->get($id);
-        if ($this->Companies->delete($company)) {
-            $this->Flash->success(__('The company has been deleted.'));
-        } else {
-            $this->Flash->error(__('The company could not be deleted. Please, try again.'));
-        }
+		$this->request->allowMethod(['post', 'delete']);
+		$company = $this->Companies->get($id);
+		if ($this->Companies->delete($company)) {
+			$this->Flash->success(__('The company has been deleted.'));
+		} else {
+			$this->Flash->error(__('The company could not be deleted. Please, try again.'));
+		}
 
-        return $this->redirect(['action' => 'index']);
-    }
+		return $this->redirect(['action' => 'index']);
+	}
+
 }
--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
@@ -12,6 +13,7 @@
  * @since     0.2.9
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
+
 namespace App\Controller;
 
 use Cake\Core\Configure;
@@ -26,45 +28,43 @@ use Cake\View\Exception\MissingTemplateException;
  *
  * @link http://book.cakephp.org/3.0/en/controllers/pages-controller.html
  */
-class PagesController extends AppController
-{
+class PagesController extends AppController {
 
-    /**
-     * Displays a view
-     *
-     * @param string ...$path Path segments.
-     * @return void|\Cake\Network\Response
-     * @throws \Cake\Network\Exception\ForbiddenException When a directory traversal attempt.
-     * @throws \Cake\Network\Exception\NotFoundException When the view file could not
-     *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
-     */
-    public function display(...$path)
-    {
-		$this->viewBuilder()->theme('default');
-        $count = count($path);
-        if (!$count) {
-            return $this->redirect('/');
-        }
-        if (in_array('..', $path, true) || in_array('.', $path, true)) {
-            throw new ForbiddenException();
-        }
-        $page = $subpage = null;
+	/**
+	 * Displays a view
+	 *
+	 * @param string ...$path Path segments.
+	 * @return void|\Cake\Network\Response
+	 * @throws \Cake\Network\Exception\ForbiddenException When a directory traversal attempt.
+	 * @throws \Cake\Network\Exception\NotFoundException When the view file could not
+	 *   be found or \Cake\View\Exception\MissingTemplateException in debug mode.
+	 */
+	public function display(...$path) {
+		$count = count($path);
+		if (!$count) {
+			return $this->redirect('/');
+		}
+		if (in_array('..', $path, true) || in_array('.', $path, true)) {
+			throw new ForbiddenException();
+		}
+		$page = $subpage = null;
 
-        if (!empty($path[0])) {
-            $page = $path[0];
-        }
-        if (!empty($path[1])) {
-            $subpage = $path[1];
-        }
-        $this->set(compact('page', 'subpage'));
+		if (!empty($path[0])) {
+			$page = $path[0];
+		}
+		if (!empty($path[1])) {
+			$subpage = $path[1];
+		}
+		$this->set(compact('page', 'subpage'));
 
-        try {
-            $this->render(implode('/', $path));
-        } catch (MissingTemplateException $e) {
-            if (Configure::read('debug')) {
-                throw $e;
-            }
-            throw new NotFoundException();
-        }
-    }
+		try {
+			$this->render(implode('/', $path));
+		} catch (MissingTemplateException $e) {
+			if (Configure::read('debug')) {
+				throw $e;
+			}
+			throw new NotFoundException();
+		}
+	}
+
 }
